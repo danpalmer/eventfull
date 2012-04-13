075c564a845a5a869b2ba171c29764dd5c6bd5b3
Dan Palmer <dan.palmer@me.com>
2012-04-13 22:09:09 +0100

    NOW serves static files

--- web/web.js
+++ web/web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express.createServer();
 
-app.use('/public', express.static(__dirname + '../public/'));
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+	res.sendfile(__dirname + '/app.html');
 });
 
 app.get('/create', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+	res.sendfile(__dirname + '/create.html');
 });
 
+app.use('/public', express.static(__dirname + '/../public/'));
 
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
