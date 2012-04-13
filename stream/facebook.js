256dd0f710691af47eed097bcf08f6fd2557c37c
Patrick Naish <pattymnaish@gmail.com>
2012-04-13 22:51:35 +0100

    Added blank response

--- stream/facebook.js
+++ stream/facebook.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/facebook', function(request, response) {
-	//response.send('Test');
+	response.send('');
 });
 
 var port = process.env.PORT || 3000;
