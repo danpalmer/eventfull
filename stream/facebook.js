49e0f07c74015d62db39656ad1d96c3da3fe3fae
Patrick Naish <pattymnaish@gmail.com>
2012-04-13 23:09:17 +0100

    Changed shit

--- stream/facebook.js
+++ stream/facebook.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 app.use(express.bodyParser());
 
 app.get('/facebook', function(request, response) {
-	response.send(request.body.hub.challenge);
-	console.log("Challenge: "+request.body.hub.challenge);
+	response.send(request.body);
+	console.log("Challenge: "+request.body);
 });
 
 var port = process.env.PORT || 3000;
