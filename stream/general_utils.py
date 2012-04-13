9f0e999005a0547a0ac27dcf9a8c607ca6c68049
Patrick Naish <pattymnaish@gmail.com>
2012-04-13 20:59:05 +0100

    Removed gravatar utils

--- stream/general_utils.py
+++
@@ -1,5 +0,0 @@
-import urllib, hashlib
-
-def getgravatar(user):
-	md5mail = hashlib.md5(user.email)
-	return 'http://www.gravatar.com/avatar.php?gravatar_id = '+md5mail+' &default = '+urllib.urlencode('http://intr.ep.io/static/images/default.png')+'&size=20&rating=G'
