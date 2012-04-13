4c5b16b63d93c731cbe72d5ad439db551271b494
Patrick Naish <pattymnaish@gmail.com>
2012-04-13 23:04:18 +0100

    changed to body parser

--- stream/facebook.js
+++ stream/facebook.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.use(express.bodyParser());
+
 app.get('/facebook', function(request, response) {
-	response.send(request.get('hub.challenge'));
-	console.log("Challenge: "+request.get('hub.challenge'));
+	response.send(request.body.hub.challenge);
+	console.log("Challenge: "+request.body.hub.challenge);
 });
 
 var port = process.env.PORT || 3000;
