ae1fbc59c8cf95a11a28500dc8cc4beafd9f23ec
Patrick Naish <pattymnaish@gmail.com>
2012-04-13 22:12:04 +0100

    Gitignore

--- stream/facebook.js
+++ stream/facebook.js
@@ -5,3 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/facebook', function(request, response) {
 	response.send('Test');
 });
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
