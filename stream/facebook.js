60a6cb4e1575dcdbcf7aff485079639dee1da22c
Patrick Naish <pattymnaish@gmail.com>
2012-04-13 22:59:04 +0100

    Added hub.challenge log

--- stream/facebook.js
+++ stream/facebook.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/facebook', function(request, response) {
 	response.send(request.get('hub.challenge'));
+	console.log("Challenge: "+request.get('hub.challenge'));
 });
 
 var port = process.env.PORT || 3000;
