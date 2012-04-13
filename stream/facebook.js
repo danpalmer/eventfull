67e8feef6f9fe65923dc0351678b273efc60ce89
Patrick Naish <pattymnaish@gmail.com>
2012-04-13 23:13:09 +0100

    AAAAAARGH

--- stream/facebook.js
+++ stream/facebook.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 app.use(express.bodyParser());
 
 app.get('/facebook', function(request, response) {
-	response.send(request.body);
-	console.log("Challenge: "+request.body);
+	response.send(request.body.hub.challenge);
+	console.log("Challenge: "+request.body.hub.challenge);
 });
 
 var port = process.env.PORT || 3000;
