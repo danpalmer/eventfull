beac0d8540220c42664cb7da425cd333852187f7
Patrick Naish <pattymnaish@gmail.com>
2012-04-14 00:59:32 +0100

    Did some stuff

--- web/web.js
+++ web/web.js
@@ -35,7 +35,7 @@ app.get('/fbredir', function(request, response){
 	var properties = querystring.stringify({
 		'client_id' : '302728933133564',
 		'redirect_uri': 'http://eventfull.herokuapp.com/authfb',
-		'state': "71862"});		
+	});		
 
 	response.statusCode = 302;
 	response.setHeader("Location", "https://www.facebook.com/dialog/oauth"+properties);
