2f17ba9ff37bd499ece8dcec9e8fec363288f500
Patrick Naish <pattymnaish@gmail.com>
2012-04-14 05:00:36 +0100

    trying horrible GET requesting

--- web/web.js
+++ web/web.js
@@ -35,7 +35,7 @@ app.post('/facebook', function(request, response){
 
 app.get('/fbredir', function(request, response){ 
 
-	response.redirect("https://www.facebook.com/dialog/oauth?client_id=302728933133564&redirect_uri=http://eventfull.herokuapp.com/authfb"); 
+	response.redirect("https://www.facebook.com/dialog/oauth?client_id=302728933133564&redirect_uri=http://eventfull.herokuapp.com/authfb&scope=user_checkins"); 
 
 });
 
