dd13a38e55387509c82ec004b06ee9bb56f0d647
Dan Palmer <dan.palmer@me.com>
2012-04-14 18:42:33 +0100

    hmmm, updating some stuffs

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: node web/web.js
 worker: node stream/twitter.js
+worker: node processor/processor.js
--- processor/processor.js
+++ processor/processor.js
@@ -3,12 +3,17 @@ var rabbitURL = process.env.CLOUDAMQP_URL || "amqp://localhost";
 var conn = amqp.createConnection({url: rabbitURL});
 
 conn.on('ready', function () {
-
 	var exchange = conn.exchange('');
-	var queue = conn.queue('activities', {}, function() {
+	var rtg   = require('url').parse(process.env.REDISTOGO_URL);
+	var redis = require('redis').createClient(rtg.port, rtg.hostname);
+	redis.auth(rtg.auth.split(':')[1]);
+	var queue = conn.queue('activities-test', {}, function() {
     queue.subscribe(function (msg) {
-      console.log(msg.body);
+      
+
+    	redis.lpush('events:1:stream', msg);
+
+
 	  });
   });
-
 });
