aa11a1ba821b9bc17f61e74ce8044df497efe131
Mark Smith <mark@markjs.net>
2012-04-14 01:18:29 +0100

    data goes into feed lol

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,27 @@
 $(function(){
-	// Hello.
+	$.get('/testdata',function(data){
+		for (var i=0; i < data.length; i++) {
+			render(data[i]);
+		};
+	})
+	
+	function render(entry) {
+		var heading, author, location
+		switch (entry.service) {
+			case "twitter":
+				heading = entry.text;
+				author = entry.user;
+				location = entry.locationText;
+				break;
+			case "facebook":
+				heading = entry.text;
+				author = entry.user;
+				location = entry.locationText;
+				break;
+		}
+		$("<article><h1>"+heading+"</h1><p class='author'>"+author+"</p><p class='location'>"+location+"</p></article>").prependTo("#stream");
+	}
+	
+	
+	
 })
