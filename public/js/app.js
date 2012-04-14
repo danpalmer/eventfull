afcd1873e5232a5ca0424baa7866f018cba2556a
Mark Smith <mark@markjs.net>
2012-04-14 10:08:05 +0100

    first stages of map stuff

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,61 @@
+function initialize() {
+	var myOptions = {
+		zoom: 16,
+		center: new google.maps.LatLng(51.522396055,-0.1098203659057),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeControl: false,
+		streetViewControl: false
+	};
+
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+	// markerImage = new google.maps.MarkerImage('/public/pin.png');
+	// markerImage.size = new google.maps.Size(32, 39);
+	// markerImage.anchor = new google.maps.Point(6, 5);
+}
+
+
 $(function(){
-	$.get('/testdata',function(data){
-		for (var i=0; i < data.length; i++) {
-			render(data[i]);
-		};
-	})
 	
+	// Get test data
+	// $.get('/testdata',function(data){
+	// 	for (var i=0; i < data.length; i++) {
+	// 		render(data[i]);
+	// 	};
+	// })
+	
+	// Render an entry
 	function render(entry) {
 		var heading, author, location
-		switch (entry.service) {
+		service = entry.service
+		switch (service) {
 			case "twitter":
 				heading = entry.text;
 				author = entry.user;
+				author_link = "http://twitter.com/"+author;
 				location = entry.locationText;
+				if (entry.mediaURL) {
+					media = entry.mediaURL;
+				}
 				break;
 			case "facebook":
 				heading = entry.text;
 				author = entry.user;
+				author_link = "http://facebook.com/"+author;
 				location = entry.locationText;
 				break;
 		}
-		$("<article><h1>"+heading+"</h1><p class='author'>"+author+"</p><p class='location'>"+location+"</p></article>").prependTo("#stream");
+		$("<article><img class='service' src='/public/images/services/"+service+".jpg'><h1>"+heading+"</h1><p class='author'><a href='"+author_link+"'>"+author+"</a></p><p class='location'>"+location+"</p></article>").prependTo("#stream");
 	}
 	
+	// Maps stuff
 	
+	
+	
+	
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize';
+	document.body.appendChild(script);	
 	
 })
