cc70e100dcbd715cb8238daae1b51cd580d53e23
Dan Palmer <dan.palmer@me.com>
2012-04-14 04:19:53 +0100

    testing twitter

---
+++ stream/twitter.js
@@ -0,0 +1,40 @@
+var twitter = require('ntwitter');
+
+var credentials = {
+  consumer_key: '9YuHRJVCsdzGxQoB1lfRrg',
+  consumer_secret: 'NjFvxvHMFApqvdFrrqiVRZXkgkGYZWLLQhZhlAz58',
+  access_token_key: '13166432-YJsxaMwDZFb6p3B33Ukp1MDGqjVk3DrXomLO79kdE',
+  access_token_secret: 'hGI2bY15GWGLS9oayYNTi03lsyE7H7fnMf4dAJZEyNI'
+}
+
+t = new twitter(credentials);
+t.stream('statuses/filter', {'track':'#ldnrealtime'}, function(stream) {
+  stream.on('data', function (data) {
+    
+  	var tweet = {};
+		tweet.user = data.user.screen_name;
+		tweet.username = data.user.name;
+		tweet.timestamp = data.created_at;
+		if (data.place) {
+			tweet.place = data.place.full_name;
+		}
+		if (data.coordinates) {
+			tweet.coordinates = data.coordinates;
+		}
+		if (data.entities) {
+			if (data.entities.media) {
+				if (data.entities.media[0].media_url) {
+					tweet.imageURL = data.entities.media[0].media_url;
+				}
+			}
+			if (data.entities.hashtags) {
+				tweet.hashtags = data.entities.hashtags;
+			}
+		}
+		tweet.service = 'twitter';
+
+		console.log(tweet);
+
+
+  });
+});
