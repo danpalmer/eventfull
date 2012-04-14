d58d852fca75ce502520d572da1bdfc804191efb
Patrick Naish <pattymnaish@gmail.com>
2012-04-14 09:10:26 +0100

    trying to fix checkin reporting

--- web/web.js
+++ web/web.js
@@ -74,7 +74,7 @@ app.post('/facebook', function(request, response){
 		res.setEncoding('utf8');
 
 		res.on("data", function(d) {
-			console.log("Got data.");
+			console.log("Got data: "+JSON.stringify(d.body));
     			data = d;
 			response.send(data);
   		});
