ea5b6c8538d95eaf3e1fc49800509233eaef8926
Patrick Naish <pattymnaish@gmail.com>
2012-04-14 06:28:17 +0100

    trying to fix checkin reporting

--- web/web.js
+++ web/web.js
@@ -33,7 +33,7 @@ app.get('/facebook', function(request, response) {
 
 app.post('/facebook', function(request, response){       
 		
-	console.log("Received POST: "+JSON.stringify(request));
+	console.log("Received POST: "+JSON.stringify(request.body));
 	
 	response.send("Thanks!");
 
