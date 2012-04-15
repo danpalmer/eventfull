9f4611ba0f7de2f295b0456824aa6a6d98afded7
Dongminator <p.donglin@gmail.com>
2012-04-15 12:01:31 +0100

    4square

--- web/web.js
+++ web/web.js
@@ -191,36 +191,6 @@ function runServer(exchange, queue) {
 
 	});
 
-	//Foursquare magic
-	app.get('/foursquare', function(request, response){
-
-		var client_id = 'DC5FCC4O4ZCIUL2XXG5ISAGVP5XM0AA4AUC1SDEJKA230QZ5';
-		var callback_url = 'http://eventfull.herokuapp.com/foursquare';
-
-		var oauth_token;
-
-		/* Attempt to retrieve access token from URL. */
-		if ($.bbq.getState('access_token')) {
-			// you get access_token
-			var token = $.bbq.getState('access_token');
-			oauth_token = token;
-			console.log(token);
-
-			$.get("https://api.foursquare.com/v2/users/self/checkins?oauth_token="+oauth_token,function(data){		
-				$.each(data['response']['checkins']['items'], function(index, result){// data.results
-					// convert string and put on message queue
-					$('#code').append("<p>"+result.venue.name+"</p>");//get all locations
-				});
-			})
-			$.bbq.pushState({}, 2);
-		} else if ($.bbq.getState('error')) {
-		} else {// you don't have access token
-			/* Redirect for foursquare authentication. */
-			window.location.href = 'https://foursquare.com/oauth2/authenticate?client_id=' + client_id
-			+ '&response_type=token&redirect_uri=' + callback_url;
-		} 
-	});
-
 	var port = process.env.PORT || 3001;
 	app.listen(port, function() {
 	  console.log("Listening on " + port);
