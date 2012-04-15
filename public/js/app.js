0b831d81319da3c40525374ea91805d9c4a238b6
Dan Palmer <dan.palmer@me.com>
2012-04-15 10:50:24 +0100

    moar

--- public/js/app.js
+++ public/js/app.js
@@ -2,28 +2,19 @@ function initialize() {
 	var myOptions = {
 		zoom: 13,
 		center: new google.maps.LatLng(51.522396055,-0.1098203659057),
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeId: google.maps.MapTypeId.TERRAIN,
 		mapTypeControl: false,
 		streetViewControl: false,
 		zoomControl: false
 	};
 
 	map = new google.maps.Map(document.getElementById('map'), myOptions);
-
-	// markerImage = new google.maps.MarkerImage('/public/pin.png');
-	// markerImage.size = new google.maps.Size(32, 39);
-	// markerImage.anchor = new google.maps.Point(6, 5);
 }
 
 
 $(function(){
 	
 	function render_loop(data){
-		// data = $.parseJSON(data);
-		// data = JSON.parse(data);
-		// data = data.replace('\\"','"');
-		// data = JSON.parse(data);
-		// console.log(data);
 		for (var i=data.length-1; i >= 0; i--) {
 			console.log("boner");
 			single = $.parseJSON(data[i]);
@@ -32,50 +23,33 @@ $(function(){
 		}
 	}
 
-	// // Get test data
-	// $.getJSON({
-	// 		url:'http://eventfull.herokuapp.com/data/1',
-	// 		success:function(data){
-	// 			render_loop(data);
-	// 		},
-	// 		error:function(data){
-	// 			console.log(data);
-	// 		},
-	// 		dataType:'jsonp'
-	// });
-
 	$.get('http://eventfull.herokuapp.com/data/1',function(data){
 		render_loop(data);
 	});
-	
-	
 	
 	// Render an entry
 	function render(entry) {
 		var heading, author, location, id, author_link, lat, lng;
 		service = entry.body.service;
+
+		heading = entry.body.text;
+		author = entry.body.user;
+		id = entry.body.id;
+
 		switch (service) {
 			case "twitter":
-				heading = entry.body.text;
-				author = entry.body.user;
-				author_link = "http://twitter.com/"+author;
+				author_link = "http://twitter.com/"+entry.body.username;
 				location = entry.body.place || "";
 				if (entry.body.coordinates) {
 					lat = entry.body.coordinates.coordinates[0];
 					lng = entry.body.coordinates.coordinates[1];
 				}
-				// location = "Locationings"
-				id = entry.body.id;
-				// id = "9001";
 				media = entry.body.imageURL || "";
 				if (media != "") {
 					media = "<img src='"+media+"'>";
 				};
-				
 				break;
 			case "facebook":
-				heading = entry.body.text;
-				author = entry.body.user;
 				author_link = "http://facebook.com/"+author;
 				location = entry.body.place || "";
 				id = entry.body.id;
@@ -83,13 +57,16 @@ $(function(){
 					lat = entry.body.coordinates['lat'];
 					lng = entry.body.coordinates['long'];
 				}
+				media = entry.body.imageURL || "";
+				if (media != "") {
+					media = "<img src='"+media.replace('s.jpg', 'n.jpg')+"'>";
+				};
 				break;
 		}
 		// Build entry article
 		$("<article id='"+service+"_"+id+"'><img class='service' src='/public/images/services/"+service+".jpg'><h1>"+heading+"</h1><p class='author'><a href='"+author_link+"'>"+author+"</a></p><p class='location'>"+location+"</p>"+media+"</article>").prependTo("#stream");
 		
 		if (lat && lng) {
-			console.log("LOCATION BONER");
 			var marker = new google.maps.Marker({
 				position: new google.maps.LatLng(lng,lat),
 				map: map,
@@ -101,25 +78,14 @@ $(function(){
 		
 		
 	}
-	
-	
-	// Pusher innit
-	
-	var pusher = new Pusher('adc860e9e73f74fd5124'); // Replace with your app key
+
+	var pusher = new Pusher('adc860e9e73f74fd5124');
 	var channel = pusher.subscribe('event_1');
 	
 	channel.bind('message', function(data) {
 		console.log("pushing...");
 	  render(data);
 	});
-	
-	
-	
-	
-	// Maps stuff
-	
-	
-	
 	
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
